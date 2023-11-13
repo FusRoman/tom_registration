f0950b1d11ae21fdaa4969a877a13367d29642f3
Roman <roman.lemontagner@gmail.com>
2023-11-13 13:50:35 +0100

    fix add user to public group

--- tom_registration/registration_flows/approval_required/forms.py
+++ tom_registration/registration_flows/approval_required/forms.py
@@ -5,17 +5,17 @@ from tom_common.forms import CustomUserCreationForm
 from django.contrib.auth.models import Group
 import logging
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
 
 class RegistrationApprovalForm(CustomUserCreationForm):
     """
     Form for handling registration requests in the approval required registration flow. Sets the user to inactive.
     """
+
     def save(self, commit=True):
         user = super().save(commit=False)
         user.is_active = False
-        if self.cleaned_data['password1']:
+
+        if self.cleaned_data["password1"]:
             user.set_password(self.cleaned_data["password1"])
         if commit:
             user.save()
@@ -28,10 +28,11 @@ class ApproveUserForm(CustomUserCreationForm):
     """
     Form for handling user registration approval requests.
     """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields.pop('password1')
-        self.fields.pop('password2')
+        self.fields.pop("password1")
+        self.fields.pop("password2")
 
     def save(self, commit=True):
         # NOTE: The superclass call is specifically to forms.ModelForm rather than CustomUserCreationForm--
@@ -39,17 +40,15 @@ class ApproveUserForm(CustomUserCreationForm):
         user = super(forms.ModelForm, self).save(commit=False)
         user.is_active = True
 
-        # --- modification from orginal ---
-        # put the user in the public group
-        logging.debug()
-        logging.debug(commit)
-        logging.debug()
-        public_group, _ = Group.objects.get_or_create(name="Public")
-        user.groups.add(public_group)
-
         if commit:
             user.save()
             self.save_m2m()
+
+        # --- modification from orginal ---
+        # put the user in the public group
+        public_group, _ = Group.objects.get_or_create(name="Public")
+        public_group.user_set.add(user)
+        public_group.save()
 
         return user
 
@@ -59,9 +58,10 @@ class ApprovalAuthenticationForm(AuthenticationForm):
     Form that replaces the default Django AuthenticationForm and renders an appropriate message if an inactive user
     attempts to log in.
     """
+
     def confirm_login_allowed(self, user):
         if not user.is_active:
             raise forms.ValidationError(
-                ('Your registration is currently pending administrator approval.'),
-                code='inactive'
+                ("Your registration is currently pending administrator approval."),
+                code="inactive",
             )
